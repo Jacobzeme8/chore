d4d4c24caa5066beaa65c4e434c1a248f4d06a46
Jacob <Jacobzeme8@gmail.com>
2023-03-20 13:27:50 -0600

    chore model done

---
+++ Models/Chore.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chore.Models
+{
+    public class Chore
+    {
+    public Chore(string name, int time, string description)
+    {
+        Name = name;
+        Time = time;
+        Description = description;
+    }
+
+    public string Name { get; set; }
+        public int Time { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; private set; } = false
+
+        public void ChoreDone(){
+            this.IsDone = true;
+        }
+    }
+}
